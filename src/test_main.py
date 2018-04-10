f4ae63ed36669bc0c4f8fddbd216fed105cf5279
Antoine <antoine-hoffmann@hotmail.com>
2018-04-10 13:51:22 +0200

    started a parallel way to compute the symmetry table

--- src/test_main.py
+++ src/test_main.py
@@ -17,13 +17,18 @@ import numpy
 import scipy.misc
 import scipy
 
+# For parallel computing
+from joblib import Parallel, delayed
+import multiprocessing
+import time
+
 
 def design_variable_merge(x, nelx, nely):
 	"""
 	Implementation of the algorithm from Kosaka and Swan 1999
-	"""			
+	"""
 	return x
-	
+
 def index_to_position(index, nelx, nely):
 	"""
 	Convert the index of a element to the centroid of the element
@@ -39,10 +44,10 @@ def add_symmetry_planes(a_array, c_array, a, c, empty = 0):
 		a_array = numpy.append(a_array,[a],0)
 		c_array = numpy.append(c_array,[c],0)
 	return a_array, c_array
-	
+
 def get_symmetry_image(a_array, c_array, nelx, nely):
 	image = numpy.ones(nelx*nely)
-	
+
 	for n in range(numpy.shape(a_array)[0]):
 		a = a_array[n]
 		c = c_array[n]
@@ -51,20 +56,47 @@ def get_symmetry_image(a_array, c_array, nelx, nely):
 			if(numpy.dot(X_i-c,a) > 0):
 				image[i] = .5;
 	return (image.reshape(nely,nelx))
-	
+
 def in_domain(X, a_array, c_array, epsilon, rmax):
 	#Check if the point X is in the domain defined by the planes
 	# Interior is for negative dot product
 	flag = True
-	for n in range(numpy.shape(a_array)[0]): 
+	for n in range(numpy.shape(a_array)[0]):
 		a = a_array[n]
 		c = c_array[n]
 		if(numpy.dot(X-c,a) > epsilon*rmax):
 			flag = False
-	
+
 	return flag
 
-def construct_connection_table(a_array, c_array, nelx, nely):	
+
+def parallelProcess(a, c, a_array, c_array, nelx, nely, rmax, epsilon_1, epsilon_2, i):
+	nmast=-1
+	X_i = index_to_position(i, nelx, nely) #Centroid of element
+
+	if(numpy.dot(X_i-c,a) < rmax * epsilon_1):
+#	if(numpy.dot(X_i-c,a) > rmax * epsilon_1):
+#			if(in_domain(X_i, a_array, c_array, epsilon_1, rmax)):
+		for n in range(numpy.shape(a_array)[0]):
+			a = a_array[n]
+			c = c_array[n]
+			X_i_proj = X_i - (numpy.dot(X_i-c,a))*a
+			dmin = rmax
+			for j in range(nelx*nely):
+				X_j = index_to_position(j, nelx, nely)
+				if(i!=j and not in_domain(X_j, a_array, c_array, epsilon_1, rmax)):
+#				if(numpy.dot(X_i-c,a) < rmax * epsilon_1):
+					temp1 = numpy.dot(X_i_proj - X_i, X_i_proj - X_j)
+					temp2 = numpy.linalg.norm(X_i_proj - X_i)*numpy.linalg.norm(X_i_proj - X_j) + epsilon_2
+					alpha_ij = temp1 / temp2
+#					print("i = ",i," j = ",j," temp1 = ",temp1, " temp2 = ", temp2, "alpha_ij = ", alpha_ij)
+					if(alpha_ij < epsilon_2-1): #Not the same as in Kosaka and Swan paper	if(alpha_ij < epsilon_2-1): #Not the same as in Kosaka and Swan paper
+						if(abs(numpy.linalg.norm(X_i_proj - X_i)-numpy.linalg.norm(X_i_proj - X_j))<dmin):
+							nmast=j
+							dmin=abs(numpy.linalg.norm(X_i_proj - X_i)-numpy.linalg.norm(X_i_proj - X_j))
+	return nmast
+
+def construct_connection_table(a_array, c_array, nelx, nely):
 	#Initialization phase
 	Xmin = index_to_position(0, nelx, nely)
 	print 'Xmin = {0}'.format(Xmin)
@@ -79,13 +111,14 @@ def construct_connection_table(a_array, c_array, nelx, nely):
 	m=0
 	alpha_ij = 0
 	connection_table = numpy.ones(nelx*nely)*-1
-		
+
+
 	for n in range(numpy.shape(a_array)[0]): #For each symmetry plane...
 		a = a_array[n]
 		c = c_array[n]
 		for i in range(nelx*nely): #1st Loop over all elements
 			X_i = index_to_position(i, nelx, nely) #Centroid of element
-			
+
 			if(numpy.dot(X_i-c,a) < rmax * epsilon_1):
 #			if(in_domain(X_i, a_array, c_array, epsilon_1, rmax)):
 				for n in range(numpy.shape(a_array)[0]):
@@ -93,7 +126,7 @@ def construct_connection_table(a_array, c_array, nelx, nely):
 					c = c_array[n]
 					k=k+1
 					X_i_proj = X_i - (numpy.dot(X_i-c,a))*a
-					dmin = rmax		
+					dmin = rmax
 					for j in range(nelx*nely):
 						X_j = index_to_position(j, nelx, nely)
 						if(i!=j and not in_domain(X_j, a_array, c_array, epsilon_1, rmax)):
@@ -107,24 +140,64 @@ def construct_connection_table(a_array, c_array, nelx, nely):
 									dmin=abs(numpy.linalg.norm(X_i_proj - X_i)-numpy.linalg.norm(X_i_proj - X_j))
 									m=m+1
 		#							print("Replacing coordinate ",i," by ",nmast," (j=",j,")")
-									connection_table[nmast]=i	
+									connection_table[nmast]=i
 	print '#elements : {0} '.format(nelx*nely)
 	print '#inside elements : {0}({1}%)'.format(k,int(100.0*k/nelx/nely))
 	print '#dependant elements : {0}({1}%)'.format(m,int(100.0*m/nelx/nely))
 	return connection_table.astype(int)
 
+
+def construct_connection_table_parallel(a_array, c_array, nelx, nely):
+	#Initialization phase
+	Xmin = index_to_position(0, nelx, nely)
+	print 'Xmin = {0}'.format(Xmin)
+	Xmax = index_to_position(nelx*nely-1, nelx, nely)
+	print 'Xmax = {0}'.format(Xmax)
+	rmax = numpy.sqrt(numpy.dot(Xmax-Xmin,Xmax-Xmin)) #Length scale of mesh
+	print 'rmax = {0}'.format(rmax)
+	epsilon_1 = 0.0000001
+	epsilon_2 = 0.0000001
+	nmast = 0
+	k=0 #Counter on elements in the interior of the plane
+	m=0
+	alpha_ij = 0
+	connection_table = numpy.ones(nelx*nely)*-1
+
+	num_cores = multiprocessing.cpu_count()
+
+	temp=[]
+	for n in range(numpy.shape(a_array)[0]): #For each symmetry plane...
+		a = a_array[n]
+		c = c_array[n]
+		temp = Parallel(n_jobs=num_cores)\
+		                  (delayed(parallelProcess)  \
+		                  (a, c, a_array, c_array, nelx, nely, rmax, epsilon_1, epsilon_2, i)\
+		                  for i in range(nelx*nely))
+		for i in range(nelx*nely):
+			if (temp[i]>-1) : connection_table[temp[i]]=i
+
+
+	return connection_table#.astype(int)
+
 def main():
+
 	from scipy import misc
+
+	#Parallel flag :
+	parallel_bool = False
+	if parallel_bool : extension_name = '_parallel_'
+	else : extension_name = ''
+
 	#Construction of a benchmark image :
-	(nelx, nely) = (64,64)
+	(nelx, nely) = (16,16)
 	img_list = numpy.ones(nelx*nely)
 	numpy.put(img_list,range(0*nelx/10 + nely/10 * nelx,10*nelx/10 + nely/10 * nelx),0)
-	
+
+	#Defining the test input :
 	for i in range(nely):
 		img_list[5*nelx/10 + i*nelx]=0
 		img_list[5*nelx/10 + i*nelx-1]=0
 		img_list[2*nelx/10 + i*nelx]=0
-	
 	img_list[5*nelx/10 + 4*nelx/10 * nelx]=0
 	img_list[7*nelx/10 + nely/10 * nelx]=0
 	img_list[6*nelx/10 + 2*nely/10 * nelx]=0
@@ -132,7 +205,7 @@ def main():
 	img_array = img_list.reshape(nely,nelx)
 	print '(nelx, nely) = {0}'.format(img_array.shape)
 	scipy.misc.toimage(img_array, cmin=0.0, cmax=1).save('output/'+str(nelx) + 'x'+str(nely) + 'image_test.png')
-	
+
 	#Symmetry planes :
 	a1 = [-nely, nelx]
 	c1 = [int(nelx/2), int(nely/2)]
@@ -143,19 +216,22 @@ def main():
 	c_array = []
 	(a_array, c_array) = add_symmetry_planes(a_array, c_array, a2, c2,1)
 	(a_array, c_array) = add_symmetry_planes(a_array, c_array, a1, c1)
-	
-	connection_table = construct_connection_table(a_array, c_array, nelx,nely)
-	
+	start = time.time()
+	if parallel_bool : connection_table = construct_connection_table_parallel(a_array, c_array, nelx,nely)
+	else : connection_table = construct_connection_table(a_array, c_array, nelx,nely)
+	end = time.time()
+	print('Ellapsed time : ' + str(end-start))
+
 	plane_image = get_symmetry_image(a_array, c_array, nelx, nely)
 	scipy.misc.toimage(plane_image, cmin=0.0, cmax=1).save('output/'+str(nelx) + 'x'+str(nely) + 'plane_image.png')
-	
-	#print(connection_table)
-	
+
+	print(numpy.array(connection_table).reshape(nelx,nely))
+
 	for i in range(nelx*nely):
 		if(connection_table[i]>-1):
 			img_list[i] = img_list[connection_table[i]]
-	
-	scipy.misc.toimage(img_list.reshape(nely,nelx), cmin=0.0, cmax=1).save('output/'+str(nelx) + 'x'+str(nely) + 'results.png')
+
+	scipy.misc.toimage(img_list.reshape(nely,nelx), cmin=0.0, cmax=1).save('output/'+str(nelx) + 'x'+str(nely) + extension_name + 'results.png')
 
 
 if __name__ == "__main__":
